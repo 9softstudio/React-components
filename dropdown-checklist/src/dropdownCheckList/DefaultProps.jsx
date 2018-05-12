10bc9517463b81c28befe0c6e88a0905ec26cc36
Phong Nguyễn <phongit.ou@gmail.com>
2018-05-13 00:17:04 +0700

    0001 move setting into others file in order to simplify main component.

---
+++ dropdown-checklist/src/dropdownCheckList/DefaultProps.jsx
@@ -0,0 +1,57 @@
+﻿export const DropdownCheckListDefaultProps = {
+    // auto select all items when load
+    selectAll: true,
+    // display text when all items are selected
+    selectAllText: "All",
+    // auto expand all items when load
+    expandAll: false,
+    // display text when no item selected
+    noSelectedText: "Please select!",
+    // display text for root node
+    rootText: "All",
+    // display text for filter text box
+    filterText: "Filter",
+    // display text when multiple items are selected
+    multipleSelectedText: "{0} item(s) selected",
+    // display text field name
+    displayName: "text",
+    // child collection field name
+    childName: "items",
+    // selection status field name
+    checkedName: "checked",
+    // expandation status field name
+    expandedName: "expanded",
+    // show root node
+    showRoot: true,
+    // show filter box
+    showFilter: true,
+    // hight of component, 0 = auto
+    height: 0,
+    // width of component, 0 = auto
+    width: 0,
+    // deplay time of typing for filter
+    filterDelay: 0.3,
+    // when data is not group, use this field to indicate parent id
+    parentIdName: false,
+    // when data is not group, use this field to indicate item id
+    idName: false,
+    // only one item is selected at time, this field accept number to indicate what child level will be single selected
+    singleSelect: false,
+    // indicate what level of item can be checked, all = -1
+    selectableLevel: -1,
+    // custom css class
+    cssClass: "",
+    // dropdown/inline mode
+    mode: "dropdown",
+    // auto check child items when parent is checked
+    autoCheck: true,
+    // to indicate what level of item to be counted
+    countLevel: -1,
+
+    dataKeyName: "$$key$$",
+    attributeKeyName: "dc-key",
+    pluginName: "dropdownCheckList",
+    listItemClassName: "ddcl__item",
+    itemCheckClassName: "ddci__check",
+    body: document.body
+ }
--- dropdown-checklist/src/dropdownCheckList/DropdownCheckList.jsx
+++ dropdown-checklist/src/dropdownCheckList/DropdownCheckList.jsx
@@ -1,6 +1,7 @@
 ﻿import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import { DropdownCheckListPropTypes } from './PropTypes';
+import { DropdownCheckListDefaultProps } from './DefaultProps';
 import Header from './Header';
 import Body from './Body';
 
@@ -18,6 +19,10 @@ export default class DropdownCheckList extends Component {
             selectedTextElement: this.props.selectAllText
         }
     }
+    
+    static propTypes = DropdownCheckListPropTypes;
+
+    static defaultProps = DropdownCheckListDefaultProps;
 
     componentWillMount() {
         var { dataSource, idName, parentIdName, countLevel, showRoot, singleSelect, autoCheck, mode } = this.props;
@@ -72,94 +77,6 @@ export default class DropdownCheckList extends Component {
             opened: false
         });
         document.removeEventListener("click", this.handleOutsideClick);
-    }
-
-    static propTypes = {
-        dropdownName: PropTypes.string.isRequired,
-        dataSource: PropTypes.arrayOf(PropTypes.object).isRequired,
-        isActive: PropTypes.bool,
-        selectAll: PropTypes.bool,
-        selectAllText: PropTypes.string,
-        expandAll: PropTypes.bool,
-        noSelectedText: PropTypes.string,
-        rootText: PropTypes.string,
-        filterText: PropTypes.string,
-        multipleSelectedText: PropTypes.string,
-        displayName: PropTypes.string,
-        childName: PropTypes.string,
-        checkedName: PropTypes.string,
-        expandedName: PropTypes.string,
-        showRoot: PropTypes.bool,
-        showFilter: PropTypes.bool,
-        height: PropTypes.number,
-        width: PropTypes.number,
-        filterDelay: PropTypes.number,
-        parentIdName: PropTypes.any,
-        idName: PropTypes.any,
-        singleSelect: PropTypes.any,
-        selectableLevel: PropTypes.number,
-        cssClass: PropTypes.string,
-        mode: PropTypes.string,
-        autoCheck: PropTypes.bool,
-        countLevel: PropTypes.number
-    }
-
-    static defaultProps = {
-        // auto select all items when load
-        selectAll: true,
-        // display text when all items are selected
-        selectAllText: "All",
-        // auto expand all items when load
-        expandAll: false,
-        // display text when no item selected
-        noSelectedText: "Please select!",
-        // display text for root node
-        rootText: "All",
-        // display text for filter text box
-        filterText: "Filter",
-        // display text when multiple items are selected
-        multipleSelectedText: "{0} item(s) selected",
-        // display text field name
-        displayName: "text",
-        // child collection field name
-        childName: "items",
-        // selection status field name
-        checkedName: "checked",
-        // expandation status field name
-        expandedName: "expanded",
-        // show root node
-        showRoot: true,
-        // show filter box
-        showFilter: true,
-        // hight of component, 0 = auto
-        height: 0,
-        // width of component, 0 = auto
-        width: 0,
-        // deplay time of typing for filter
-        filterDelay: 0.3,
-        // when data is not group, use this field to indicate parent id
-        parentIdName: false,
-        // when data is not group, use this field to indicate item id
-        idName: false,
-        // only one item is selected at time, this field accept number to indicate what child level will be single selected
-        singleSelect: false,
-        // indicate what level of item can be checked, all = -1
-        selectableLevel: -1,
-        // custom css class
-        cssClass: "",
-        // dropdown/inline mode
-        mode: "dropdown",
-        // auto check child items when parent is checked
-        autoCheck: true,
-        // to indicate what level of item to be counted
-        countLevel: -1,
-
-        dataKeyName: "$$key$$",
-        attributeKeyName: "dc-key",
-        pluginName: "dropdownCheckList",
-        listItemClassName: "ddcl__item",
-        itemCheckClassName: "ddci__check",
-        body: document.body
     }
 
     //#region helpers
