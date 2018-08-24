f642e48699c4a2ded4d20117c1080763b909b142
Walter <walter.tran@starixsoft.com>
2018-08-24 18:25:18 +0700

    Export Pager component

--- react-fixed-table-header/src/index.jsx
+++ react-fixed-table-header/src/index.jsx
@@ -1,5 +1,6 @@
 ﻿import Table from './components/Table';
 import Row from './components/Row';
 import Cell from './components/Cell';
+import Pager from './components/Pager';
 
-export { Table, Row, Cell }
+export { Table, Row, Cell, Pager }
