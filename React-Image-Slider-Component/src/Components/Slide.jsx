4da543da354a392df667454ff34561d5d1ef5947
Parsons Nguyen <parsons.nguyen@nexcel.vn>
2018-09-10 16:09:32 +0700

    support image webp with .jpg fallback.

--- React-Image-Slider-Component/src/Components/Slide.jsx
+++ React-Image-Slider-Component/src/Components/Slide.jsx
@@ -4,14 +4,16 @@ import Context from '../slider-context';
 export default class Slide extends Component {
     render() {
         const { imageInfo, style, index } = this.props;
+        const extensionOfImage = imageInfo.url.substring(imageInfo.url.lastIndexOf('.'));
         return (
             <Context.Consumer>{
                 ({ onImageClick }) =>
-                    <img src={imageInfo.url}
-                        alt={imageInfo.text}
-                        className="slide"
+                    <img className="slide"
+                        style={style}
                         onClick={() => onImageClick(index)}
-                        style={style} />
+                        alt={imageInfo.text}
+                        src={imageInfo.url}
+                        onError={e => e.target.src = imageInfo.url.replace(extensionOfImage, '.jpg')} />
             }</Context.Consumer>);
     }
 };
