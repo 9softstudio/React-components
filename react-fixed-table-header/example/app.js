383606e0eb426d72c84deec91a2b4e60f7c1b636
Walter <walter.tran@starixsoft.com>
2018-10-08 18:17:47 +0700

    Reproduce error by example

--- react-fixed-table-header/example/app.js
+++ react-fixed-table-header/example/app.js
@@ -13,15 +13,55 @@ const defaultPagingOption = {
 export default class App extends Component {
     constructor(props) {
         super(props);
+
+        this.addMoreCell = true;
     }
 
+    // _buildHeader() {
+    //     return (<Row style={{ height: 32 }}>
+    //         <Cell colWidth={150} header={true}>Header 1</Cell>
+    //         <Cell colWidth={150} header={true}>Header 2</Cell>
+    //         <Cell colWidth={200} header={true}>Header 3</Cell>
+    //         <Cell colWidth={150} header={true}>Header 4</Cell>
+    //         {this.addMoreCell ? (<Cell colWidth={150} header={true}>Header 5</Cell>) : null}
+    //     </Row>);
+    // }
+
     _buildHeader() {
-        return (<Row style={{ height: 32 }}>
-            <Cell colWidth={150} header={true}>Header 1</Cell>
-            <Cell colWidth={150} header={true}>Header 2</Cell>
-            <Cell colWidth={200} header={true}>Header 3</Cell>
-            <Cell colWidth={150} header={true}>Header 4</Cell>
-        </Row>);
+        const isMainView = false;
+        const firstColName = isMainView ? 'Affiliate' : 'Month';
+
+        return [
+            <Row>
+                <Cell header={true} colWidth={120} rowSpan="2" title={firstColName}>{firstColName}</Cell>
+                <Cell header={true} colWidth={70} rowSpan="2" title="Total Click">Total Click</Cell>
+                <Cell header={true} colWidth={70} rowSpan="2" title="Unique Click">Unique Click</Cell>
+                <Cell header={true} colWidth={65} rowSpan="2" title="Signup">Signup</Cell>
+                <Cell header={true} colWidth={50} rowSpan="2" title="FDM">FDM</Cell>
+                <Cell header={true} colWidth={60} rowSpan="2" title="Active Member">Active Member</Cell>
+                <Cell header={true} colWidth={60} rowSpan="2" title="Qualified Member">Qualified Member</Cell>
+                <Cell header={true} colSpan="9" title="Member Statistics">Member Statistics</Cell>
+                <Cell header={true} colSpan={isMainView ? 5 : 4} title="Affiliate Statistics">Affiliate Statistics</Cell>
+                {isMainView && (<Cell header={true} colWidth={50} rowSpan="2" title="Month on Month">Month on Month</Cell>)}
+            </Row>,
+            <Row>
+                <Cell header={true} colWidth={75} title="Deposit">Deposit</Cell>
+                <Cell header={true} colWidth={75} title="Withdrawal">Withdrawal</Cell>
+                <Cell header={true} colWidth={95} title="Turnover">Turnover</Cell>
+                <Cell header={true} colWidth={95} title="WinLoss">WinLoss</Cell>
+                <Cell header={true} colWidth={70} title="Bonus">Bonus</Cell>
+                <Cell header={true} colWidth={70} title="Platform Fee">Platform Fee</Cell>
+                <Cell header={true} colWidth={70} title="Operational Fee">Operational Fee</Cell>
+                <Cell header={true} colWidth={70} title="Banking Fee">Banking Fee</Cell>
+                <Cell header={true} colWidth={70} title="Total Fee">Total Fee</Cell>
+
+                <Cell header={true} colWidth={60} title="Comm%">Comm%</Cell>
+                <Cell header={true} colWidth={80} title="Earning">Earning</Cell>
+                <Cell header={true} colWidth={80} title="Withdrawal">Withdrawal</Cell>
+                {isMainView && (<Cell header={true} colWidth={70} title="Adjustment">Adjustment</Cell>)}
+                <Cell header={true} colWidth={100} title="Balance B/F">Balance B/F</Cell>
+            </Row>
+        ];
     }
 
     _buildBody() {
@@ -32,6 +72,7 @@ export default class App extends Component {
                 <Cell>Data 2.0</Cell>
                 <Cell>Data 3.0</Cell>
                 <Cell>Data 4.0</Cell>
+                {this.addMoreCell ? (<Cell>Data 5.0</Cell>) : null}
             </Row>
         );
         rows.push(
@@ -39,6 +80,7 @@ export default class App extends Component {
                 <Cell>Data 2.1</Cell>
                 <Cell>Data 3.1</Cell>
                 <Cell>Data 4.1</Cell>
+                {this.addMoreCell ? (<Cell>Data 5.1</Cell>) : null}
             </Row>
         );
 
@@ -49,6 +91,7 @@ export default class App extends Component {
                     <Cell>Data 2</Cell>
                     <Cell>Data 3</Cell>
                     <Cell>Data 4</Cell>
+                    {this.addMoreCell ? (<Cell>Data 5</Cell>) : null}
                 </Row>
             );
         }
@@ -65,6 +108,7 @@ export default class App extends Component {
                     <Cell>Footer 2</Cell>
                     <Cell>Footer 3</Cell>
                     <Cell>Footer 4</Cell>
+                    {this.addMoreCell ? (<Cell>Footer 5</Cell>) : null}
                 </Row>
             );
         }
@@ -79,8 +123,8 @@ export default class App extends Component {
                 autoWidth={true}
                 bodyHeight={300}
                 header={this._buildHeader()}
-                body={this._buildBody()}
-                footer={this._buildFooter()}
+                body={[]}
+                footer={[]}
                 isPaging={true}
                 pageOption={defaultPagingOption}
                 containerPadding={0}
