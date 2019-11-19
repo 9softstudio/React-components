281e51d309c95506d9d7b6909bcf1ac4502cf683
Walter <walter.tran@starixsoft.com>
2019-11-19 16:25:03 +0700

    Correct getting parentKey value

--- react-multiselect/src/utils.js
+++ react-multiselect/src/utils.js
@@ -1,19 +1,20 @@
 import { KEY_NAME, VALUE_NAME, STATUS_NAME, defaultTreeViewOption } from './constans';
 
-export const convertDataSourceToState = ({ keyField, valueField, statusField, dataSource, treeViewOption }, parent, level = 0) => {
+export const convertDataSourceToState = ({ keyField, valueField, statusField, dataSource, treeViewOption }, parentKey = null, level = 0) => {
     const actualTreeViewOption = (treeViewOption && {...defaultTreeViewOption, ...treeViewOption }) || {...defaultTreeViewOption };
 
     const result = [];
     for (let i = 0; i < dataSource.length; i++) {
         const item = dataSource[i];
-        result.push({
+        const convertedItem = {
             [KEY_NAME]: item[keyField],
             [VALUE_NAME]: item[valueField],
             [STATUS_NAME]: !!item[statusField],
             visible: true,
             level: level,
-            parentKey: level === 0 ? null : parent && parent[keyField]
-        });
+            parentKey: parentKey
+        }
+        result.push(convertedItem);
 
         if (actualTreeViewOption && item[actualTreeViewOption.childrenField]) {
             const children = item[actualTreeViewOption.childrenField];
@@ -26,7 +27,7 @@ export const convertDataSourceToState = ({ keyField, valueField, statusField, da
                 treeViewOption: actualTreeViewOption
             };
             const currentLevel = level + 1;
-            const itemList = convertDataSourceToState(param, item, currentLevel);
+            const itemList = convertDataSourceToState(param, convertedItem[KEY_NAME], currentLevel);
             result.push(...itemList);
         }
     }
